2f08730bb61b4b59d680692a287f7f5cc82d642e
muknan <muk.msi@gmail.com>
2023-08-18 14:53:50 +0530

    Added search with  barebone functionality

--- js/script.js
+++ js/script.js
@@ -2,27 +2,61 @@ const userEl = document.querySelector(".user-nav__user");
 const userListEl = document.querySelector(".user-nav__user-list");
 const userMenuEl = document.querySelector(".user-nav__menu");
 
-// userEl.addEventListener("click", function () {
-//   userListEl.style.cssText = `opacity: 1;
-//   visibility: visible;
-//   top: 7rem;
-// `;
+const searchInput = document.querySelector(".search__input");
+const searchResults = document.querySelector(".search__results");
 
-//   //   if (
-//   //     userListEl.style.opacity === "0" &&
-//   //     userListEl.style.visibility === "hidden"
-//   //   ) {
-//   //     console.log("hello");
-//   //     userListEl.style.cssText = `opacity: 1;
-//   //   visibility: visible;
-//   // `;
-//   //   } else {
-//   //     console.log("world");
-//   //     userListEl.style.cssText = `opacity: 0;
-//   //   visibility: hidden;
-//   // `;
-//   //   }
-// });
+const searchData = [
+  "Antigonish Evergreen Inn",
+  "Microtel Inn & Suites by Wyndham Antigonish",
+  "Maritime Inn Antigonish",
+  "Hotel Victorian Inn",
+  "Homeward Inns of Canada",
+  "Oasis Motel And Campground",
+  "Coastal Inn Antigonish",
+  "The Cove Motel and Restaurant",
+  "Chisholm's of Troy Coastal Cottages",
+  "Sandstone Bed & Breakfast",
+];
+
+function filterResults(query) {
+  return searchData.filter((item) =>
+    item.toLowerCase().includes(query.toLowerCase())
+  );
+}
+
+function displayResults(results) {
+  searchResults.innerHTML = "";
+  results.forEach((result) => {
+    let li = document.createElement("li");
+    li.textContent = result;
+    li.addEventListener("click", () => {
+      searchInput.value = result;
+      searchResults.innerHTML = "";
+    });
+    searchResults.appendChild(li);
+  });
+}
+
+// event listener for search input
+searchInput.addEventListener("input", () => {
+  let query = searchInput.value;
+  if (query === "") {
+    searchResults.innerHTML = "";
+    console.log("Hello");
+  } else if (query.length > 1) {
+    let results = filterResults(query);
+    displayResults(results);
+  }
+});
+
+// event listener to close search results when clicking outside the input and the results
+document.addEventListener("click", (event) => {
+  let isClickInsideInput = event.target === searchInput;
+  let isClickInsideResults = searchResults.contains(event.target);
+  if (!isClickInsideInput && !isClickInsideResults) {
+    searchResults.innerHTML = "";
+  }
+});
 
 userEl.addEventListener("click", function () {
   userListEl.classList.toggle("menu-toggle"), false;
@@ -35,3 +69,13 @@ userMenuEl.addEventListener(
   },
   false
 );
+
+userListEl.addEventListener("mouseover", function () {
+  userEl.style.cssText = `
+  background-color: #fff;
+`;
+});
+
+userListEl.addEventListener("mouseout", function () {
+  userEl.style.backgroundColor = "";
+});
